0c49d5f69be300f5643492b3bd6389c035574348
Alex Hough <ahough@repay.com>
2020-01-26 05:34:49 -0700

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,34 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-universal-theme# if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
-
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
+
+# Committing to dependencies codebase as well
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
 git push origin master
