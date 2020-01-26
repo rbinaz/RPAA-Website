56ce11e85611c90de5e4697da3d1e3692f39b4d8
Alex Hough <ahough@repay.com>
2020-01-26 05:48:18 -0700

    rebuilding site Sun Jan 26 05:48:18 MST 2020

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t hugo-universal-theme# if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hugo-universal-theme
 
 # Go To Public folder
 cd public
